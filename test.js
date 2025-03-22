2756ab35b8341a3c7aa3ef907ea3f54a28dbad4a
besioo <61539608+besioo@users.noreply.github.com>
2025-03-21 18:23:11 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-alert(0);
+fetch('https://www.adidas.com/api/account/profile?sitePath=us', {
+  method: 'GET', // or 'POST', 'PUT', etc.
+  credentials: 'include' // Ensures cookies are sent
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
